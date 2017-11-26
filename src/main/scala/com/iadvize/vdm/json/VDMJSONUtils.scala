2456f2398959263145cc954fce0d4afd70c05c7a
Alexandre Garbe <alex@MacBook-Pro-de-Alexandre.local>
2017-11-27 00:39:27 +0100

    Added missing method comments to VDMJSONUtils

--- src/main/scala/com/iadvize/vdm/json/VDMJSONUtils.scala
+++ src/main/scala/com/iadvize/vdm/json/VDMJSONUtils.scala
@@ -1,7 +1,7 @@
 package com.iadvize.vdm.json
 
 import com.iadvize.vdm.scrapper.VDMPost
-import org.json4s.jackson.Serialization.{write,read}
+import org.json4s.jackson.Serialization.{read, write}
 
 /**
   * Object class used to convert VDMPost objects to and from JSON format
@@ -10,13 +10,38 @@ object VDMJSONUtils {
 
   private implicit val formats = org.json4s.DefaultFormats ++ org.json4s.ext.JodaTimeSerializers.all
 
-  def toJSON(posts: List[VDMPost]): String = write(("posts",posts))
+  /**
+    * Takes a list of [[VDMPost]] and convert them to a json doc
+    *
+    * @param posts a [[VDMPost]] List
+    * @return a json document
+    */
+  def toJSON(posts: List[VDMPost]): String = write(("posts", posts))
 
-  def toJSONWithPostCount(posts: List[VDMPost]): String = write(Map("posts" -> posts,"count" -> posts.size))
+  /**
+    * Takes a list of [[VDMPost]] and convert them to a json doc, adding a "count" field
+    *
+    * @param posts a [[VDMPost]] List
+    * @return a json document
+    */
+  def toJSONWithPostCount(posts: List[VDMPost]): String = write(Map("posts" -> posts, "count" -> posts.size))
 
-  def toJSON(post: VDMPost): String = write(("post",post))
+  /**
+    * Takes a [[VDMPost]] and return a json document
+    *
+    * @param post a [[VDMPost]]
+    * @return a json document
+    */
+  def toJSON(post: VDMPost): String = write(("post", post))
 
-  def fromJSON(jsonPosts: String): List[VDMPost] = read[Tuple2[String,List[VDMPost]]](jsonPosts)._2
+  /**
+    * Takes a json document containing several posts (without the "count" field) and convert them to a List
+    * of [[VDMPost]]
+    *
+    * @param jsonPosts the json document
+    * @return a list of [[VDMPost]]
+    */
+  def fromJSON(jsonPosts: String): List[VDMPost] = read[Tuple2[String, List[VDMPost]]](jsonPosts)._2
 
 
 }
