b74c246703ea29f7d3691a5321a62cd556d3ee76
Alexandre Garbe <alex@MacBook-Pro-de-Alexandre.local>
2017-11-27 11:17:15 +0100

    Added logs on VDMSiteScrapper

--- src/main/scala/com/iadvize/vdm/scrapper/VDMSiteScrapper.scala
+++ src/main/scala/com/iadvize/vdm/scrapper/VDMSiteScrapper.scala
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths}
 
 import com.iadvize.vdm.json.VDMJSONUtils
+import com.typesafe.scalalogging.Logger
 
 /**
   * Main class for http://www.viedemerde.fr.
@@ -13,9 +14,15 @@ import com.iadvize.vdm.json.VDMJSONUtils
 object VDMSiteScrapper extends App {
 
 
+  private val logger = Logger[VDMPageParser]
+
   val paginatedParser = new VDMPaginatedParser(new VDMPageParser(new VDMPostParser))
 
+  logger.info("Scrapping started")
+
   val jsonPosts = VDMJSONUtils.toJSON(paginatedParser.parse(200))
+
+  logger.info("Scrapping ended")
 
   Files.write(Paths.get("vdm_posts.json"), jsonPosts.getBytes(StandardCharsets.UTF_8))
 
